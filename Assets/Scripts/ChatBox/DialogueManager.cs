3fb5433729f437eea26d84c7173d361dc922fff5
Jan Benko <jan.benko777@gmail.com>
2024-09-03 16:40:07 +0200

    ChatBox First Prototype
    First prototype finished, but will very likely have to switch to a node based graph for multiple dialogue options.

---
+++ Assets/Scripts/ChatBox/DialogueManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+
+    public Image characterIcon;
+    public TextMeshProUGUI characterName;
+    public TextMeshProUGUI dialogueArea;
+
+    private Queue<DialogueLine> lines;
+
+    public bool isDialogueActive = false;
+
+    public float typingSpeed = 0.2f;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        isDialogueActive = true;
+        animator.SetTrigger("TrOpen");
+
+        lines = new Queue<DialogueLine>();
+
+        foreach (DialogueLine line in dialogue.dialogueLines)
+        {
+            lines.Enqueue(line);
+        }
+
+        DisplayNextDialogueLine();
+    }
+
+    public void DisplayNextDialogueLine()
+    {
+        if (lines.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        DialogueLine currentLine = lines.Dequeue();
+
+        characterIcon.sprite = currentLine.character.icon;
+        characterName.text = currentLine.character.name;
+        dialogueArea.text = "";
+
+        StopAllCoroutines();
+
+        StartCoroutine(TypeSentence(currentLine));
+    }
+
+    IEnumerator TypeSentence(DialogueLine dialogueLine)
+    {
+        dialogueArea.text = "";
+
+        foreach (char letter in dialogueLine.line.ToCharArray())
+        {
+            dialogueArea.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void EndDialogue()
+    {
+        isDialogueActive = false;
+        animator.SetTrigger("TrClose");
+    }
+}
