bd2541d86ca5466309352e19e3721f7386a9e560
JustFunPlay <dinandmeijer01@gmail.com>
2024-09-04 14:33:18 +0200

    Hide from view improvements

--- Assets/Dinand scripts/HideFromView.cs
+++ Assets/Dinand scripts/HideFromView.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class HideFromView : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
-    [SerializeField] private float maxBound;
+    [SerializeField] private float boundModifier;
     enum BoundDirection
     {
         posX,
@@ -14,26 +14,102 @@ public class HideFromView : MonoBehaviour
         posZ
     }
     [SerializeField] private BoundDirection boundDirection;
-    [SerializeField] private GameObject[] objectsToHide;
+    [SerializeField] public List<HideObject> hideObjects {  get; private set; }
+    [SerializeField] private List<HideFromView> childHiders = new List<HideFromView>();
 
-    private void FixedUpdate()
+    public bool disabledByParent;
+    bool wasVisible;
+
+    private void Awake()
     {
-        bool isvisible = true;
+        playerTransform = FindObjectOfType<PlayerInput>().transform;
+        hideObjects = new List<HideObject>();
+        hideObjects.AddRange(GetComponentsInChildren<HideObject>());
+        childHiders.AddRange(GetComponentsInChildren<HideFromView>());
+        childHiders.Remove(this);
+        foreach (HideFromView hfv in childHiders)
+        {
+            for (int i = 0; i < hfv.hideObjects.Count; i++)
+            {
+                hideObjects.Remove(hfv.hideObjects[i]);
+            }
+        }
+
+        bool isVisible = true;
         switch (boundDirection)
         {
             case BoundDirection.posX:
-                if (playerTransform.position.x > maxBound) isvisible = false;
+                if (playerTransform.position.x > transform.position.x + boundModifier) isVisible = false;
                 break;
             case BoundDirection.negY:
-                if (playerTransform.position.y < maxBound) isvisible = false;
+                if (playerTransform.position.y < transform.position.y + boundModifier) isVisible = false;
                 break;
             case BoundDirection.posZ:
-                if (playerTransform.position.z > maxBound) isvisible = false;
+                if (playerTransform.position.z > transform.position.z + boundModifier) isVisible = false;
                 break;
         }
-        for (int i = 0;i < objectsToHide.Length; i++)
+        for (int i = 0; i < hideObjects.Count; i++)
         {
-            objectsToHide[i].SetActive(isvisible);
+            hideObjects[i].RevealImmediately(isVisible);
+        }
+        foreach (HideFromView hfv in childHiders)
+        {
+            hfv.disabledByParent = !isVisible;
+            for (int i = 0; i < hfv.hideObjects.Count; i++)
+            {
+                hfv.hideObjects[i].RevealImmediately(isVisible);
+            }
+        }
+        wasVisible = isVisible;
+    }
+
+    private void FixedUpdate()
+    {
+        if (disabledByParent) return;
+        bool isVisible = true;
+        switch (boundDirection)
+        {
+            case BoundDirection.posX:
+                if (playerTransform.position.x > transform.position.x + boundModifier) isVisible = false;
+                break;
+            case BoundDirection.negY:
+                if (playerTransform.position.y < transform.position.y + boundModifier) isVisible = false;
+                break;
+            case BoundDirection.posZ:
+                if (playerTransform.position.z > transform.position.z + boundModifier) isVisible = false;
+                break;
+        }
+        if (isVisible != wasVisible)
+        {
+            for (int i = 0; i < hideObjects.Count; i++)
+            {
+                if (isVisible)
+                    hideObjects[i].Show();
+                else
+                    hideObjects[i].Hide();
+            }
+            wasVisible = isVisible;
+
+            foreach (HideFromView hfv in childHiders)
+            {
+                hfv.disabledByParent = !isVisible;
+                hfv.SequenceCheck(isVisible);
+            }
         }
     }
+
+    public void SequenceCheck(bool isVisible)
+    {
+        if (isVisible != wasVisible)
+        {
+            for (int i = 0; i < hideObjects.Count; i++)
+            {
+                if (isVisible)
+                    hideObjects[i].Show();
+                else
+                    hideObjects[i].Hide();
+            }
+            wasVisible = isVisible;
+        }
+        }
 }
---
+++ Assets/Dinand scripts/HideObject.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObject : MonoBehaviour
+{
+    [SerializeField] private Material tpMaterial;
+    [SerializeField] private Material opMaterial;
+    
+    Color opacityColor;
+    bool revealing;
+    MeshRenderer mRenderer;
+    Collider mCollider;
+    private const float minOpacity = 0.2f;
+
+    private void Awake()
+    {
+        mRenderer = GetComponent<MeshRenderer>();
+        mCollider = GetComponent<Collider>();
+        opacityColor = mRenderer.material.color;
+
+    }
+    public void Show()
+    {
+        revealing = true;
+    }
+    public void Hide()
+    {
+        revealing = false;
+    }
+
+    public void RevealImmediately(bool reveal)
+    {
+        revealing = reveal;
+        opacityColor.a = reveal? 1 : minOpacity;
+        if (reveal)
+        {
+            mRenderer.material = opMaterial;
+            mRenderer.material.color = opacityColor;
+            //mRenderer.enabled = true;
+            if (mCollider != null) mCollider.enabled = true;
+        }
+        else
+        {
+            mRenderer.material = tpMaterial;
+            mRenderer.material.color = opacityColor;
+            //mRenderer.enabled = false;
+            if (mCollider != null) mCollider.enabled = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if ((opacityColor.a <= minOpacity && !revealing) || (opacityColor.a == 1 && revealing)) return;
+
+        if (revealing)
+        {
+            //mRenderer.enabled = true;
+            if (mCollider != null) mCollider.enabled = true;
+
+            opacityColor.a = Mathf.Clamp(opacityColor.a + 0.05f, minOpacity, 1);
+            if (opacityColor.a == 1)
+                mRenderer.material = opMaterial;
+            
+            mRenderer.material.color = opacityColor;
+        }
+        else if (!revealing)
+        {
+            if (opacityColor.a == 1)
+                mRenderer.material = tpMaterial;
+            
+            opacityColor.a = Mathf.Clamp(opacityColor.a - 0.05f, minOpacity, 1);
+
+            mRenderer.material.color = opacityColor;
+
+            if (opacityColor.a == 0)
+            {
+                //mRenderer.enabled = false;
+                if (mCollider != null) mCollider.enabled = false;
+            }
+        }
+    }
+}
